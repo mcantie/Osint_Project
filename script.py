0a5af1a7788178dfa7899184cfcb3d6662dbb559
mcantie <conterstrek@gmail.com>
2022-06-30 18:31:33 +0200

    filtering of IOC type into dif tables

--- script.py
+++ script.py
@@ -1,22 +1,66 @@
 import csv
 import codecs
 import whois
+import re
+
 from geoip import geolite2
 
-rows = []
+ip = []
+domains = []
+sample = "sample.csv"
 
-with open("sample.csv", 'r') as file:
-    csvreader = csv.reader(codecs.EncodedFile(file, 'utf-8', 'utf-8-sig'), delimiter=";")
-    for row in csvreader:
-        rows.append(row)
+regexip = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+regexdomains = "[A-Za-z0-9]{1,63}\.[a-z]{2,3}"
 
-print(rows)
 
-match = geolite2.lookup('8.8.8.8')
-match.country
+def printAscii():
+	"""
+	ASCII Art
+	"""
+	print("""
+  _____ ____   _____  _____ _____ _   _ _______ 
+ |_   _/ __ \ / ____|/ ____|_   _| \ | |__   __|
+   | || |  | | |    | (___   | | |  \| |  | |   
+   | || |  | | |     \___ \  | | | . ` |  | |   
+  _| || |__| | |____ ____) |_| |_| |\  |  | |   
+ |_____\____/ \_____|_____/|_____|_| \_|  |_|                                                                                                                                                                												  
+	
+    """)
 
-print(match.country)
 
-w = whois.whois('utt.fr')
+def extract_csv(csvfile, regexip, regexdomains):
 
-print(w)
+    with open(csvfile, 'r') as file:
+        csvreader = csv.reader(codecs.EncodedFile(file, 'utf-8', 'utf-8-sig'), delimiter=";")
+        for row in csvreader:
+            for cell in row:
+                match_ip = re.match(regexip, cell)
+                match_domains = re.match(regexdomains, cell)
+                if match_ip:
+                    ip.append(row)
+                if match_domains:
+                    domains.append(row)
+
+    print("ip :",ip)
+    print("domains :", domains)
+        
+
+def geoip():
+    match = geolite2.lookup('8.8.8.8')
+    match.country
+    print(match.country)
+
+
+def domain():
+    w = whois.whois('utt.fr')
+    print(w)
+
+
+def main():
+
+    printAscii()
+    extract_csv(sample, regexip, regexdomains)
+    
+
+if __name__ == '__main__':
+	main()
